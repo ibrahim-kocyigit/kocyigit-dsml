187b6ec5a8b8ba13672222b139727a02b1b9bea2
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-04-30 08:58:44 +0300

    working on numpy cheatsheet

--- 02-practice/numpy_cheatsheet.py
+++ 02-practice/numpy_cheatsheet.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+np.info()  # Get help information for an array, function, class, or module.
+
+
 # ----------------------- CREATING ARRAYS ----------------------- #
 a = np.array([1, 2, 3])
 b = np.array([(1.5, 2, 3), (4, 5, 6)], dtype=float)
@@ -14,6 +17,7 @@ f = np.eye(2)  # Create a 2x2 identity matrix
 np.random.random((2, 2))  # Create a 2x2 array with random values
 np.empty((3, 2))  # Create a 3x2 empty array
 
+
 # ----------------------------- I/O ----------------------------- #
 # Saving & Loading on Disk
 np.save("my_array", a)  # Save an array to a binary file in NumPy .npy format.
@@ -24,9 +28,52 @@ np.load(
     "my_array.npy"
 )  # Loads arrays or pickled objects from .npy, .npz or pickled files.
 
-# ----------------- Saving & Loading Text Files ----------------- #
+
+# ----------------- SAVING & LOADING TEXT FILES ----------------- #
 np.loadtxt("myfile.txt")  # Load data from a text file.
 np.genfromtxt(
     "my_file.csv", delimiter=","
 )  # Load data from a text file, with missing values handled as specified.
 np.savetxt("myarray.txt", a, delimiter=" ")  # Save an array to a text file.
+
+
+# -------------------- INSPECTING YOUR ARRAY -------------------- #
+a.shape  # Array dimensions
+len(a)  # Length of array
+b.ndim  # Number of array dimensions
+e.size  # Number of array elements
+b.dtype  # Data type of array elements
+b.dtype.name  # Name of data type
+b.astype(int)  # Convert an array to a different type
+
+
+# ------------------------- ARRAY MATHS ------------------------- #
+g = a - b  # Subtraction
+np.subtract(a, b)  # Subtraction
+b + a  # Addition
+np.add(b, a)  # Addition
+a / b  # Division
+np.divide(a, b)  # Division
+a * b  # Multiplication
+np.multiply(a, b)  # Multiplication
+np.exp(b)  # Exponentiation
+np.sqrt(b)  # Square root
+np.sin(a)  # Print sines of an array
+np.cos(b)  # Element-wise cosine
+np.log(a)  # Element-wise natural logratihm
+f.dot(g)  # Dot product
+f @ g  # Dot product
+np.dot(f, g)  # Dot product
+
+
+# ------------------------- COMPARISON -------------------------- #
+a == b  # Element-wise comparison. Returns array of bools.
+a < 2  # Element-wise comparison. Returns array of bools.
+np.array_equal(a, b)  # Array-wise comparison. Returns bool.
+
+
+# --------------------- AGGREGATE FUNCTIONS --------------------- #
+a.sum()  # Array-wise sum
+a.min()  # Array-wise minimum value
+b.max(axis=0)  # Maximum value of an array row
+b.cumsum(axis=1)  # Cumulative sum of the elements as an array
