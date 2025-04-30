811f1fa8bb022a06fee0ceafc628ef7ece88a8dd
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-04-30 09:31:35 +0300

    working on numpy cheatsheet

--- 02-practice/numpy_cheatsheet.py
+++ 02-practice/numpy_cheatsheet.py
@@ -77,3 +77,60 @@ a.sum()  # Array-wise sum
 a.min()  # Array-wise minimum value
 b.max(axis=0)  # Maximum value of an array row
 b.cumsum(axis=1)  # Cumulative sum of the elements as an array
+
+
+# ----------------------- COPYING ARRAYS ------------------------ #
+h = a.view()  # Create a view of the array with the same data
+np.copy(a)  # Create a copy of the array
+h = a.copy()  # Create a deep copy of the array
+
+
+# ----------------------- SORTING ARRAYS ------------------------ #
+a.sort()  # Sort an array
+c.sort(axis=0)  # Sort the elements of an array's axis
+
+
+# ---------------- SUBSETTING, SLICING, INDEXING ---------------- #
+# Subsetting:
+a[2]  # Select the element at the 2nd index
+b[1, 2]  # Select the element at row 1, column 2 ( = b[1][2])
+
+# Slicing:
+a[0:2]  # Select items at index 0 and 1
+b[0:2, 1]  # Select items at rows 0 and 1 in column 1
+b[:1]  # Select all items at row 0 ( = b[0:1, :])
+c[1, ...]  # Same as [1,:,:]
+a[::-1]  # Reversed
+
+# Boolean Indexing:
+a[a < 2]  # Selects elements from a less than 2
+
+# Fancy Indexing:
+b[[1, 0, 1, 0], [0, 1, 2, 0]]  # Select elements (1,0), (0,1), (1,2), (0,0)
+b[[1, 0, 1, 0]][:, [0, 1, 2, 0]]  # Select a subset of matrix's rows and columns
+
+
+# --------------------- ARRAY MANIPULATION ---------------------- #
+# Transposing Array:
+i = np.transpose(b)  # Permute array dimensions
+i.T  # Permute array dimensions
+
+# Changing Array Shape:
+b.ravel()  # Flatten the array
+g.reshape(3, -2)  # Reshape, but don't change the data
+
+# Adding/Removing Elements
+f.resize((2, 6))  # Return a new array with shape (2, 6)
+np.append(h, g)  # Append items in an array
+np.insert(a, 1, 5)  # Insert values along the given axis before the given indices.
+np.delete(a, [1])  # Delete items from an array
+
+# Combining Arrays
+np.concatenate((a, d), axis=0)  # Concatenate arrays
+np.vstack((a, b))  # Stack arrays vertically (row-wise)
+np.hstack((f, g))  # Stack arrays horizontally (column-wise)
+np.column_stack((f, g))  # Used when 1D arrays become columns in the output.
+
+# Splitting Arrays
+np.hsplit(g, 3)  # Split the array horizontally at the 3rd index
+np.vsplit(c, 1)  # Sptlit the array vertically at the 1st index
