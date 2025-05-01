028c5174b136d5f814478074f62ace657f3ae6d2
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-05-01 20:34:39 +0300

    added python basics script

--- 02-practice/01_python/01_basics.py
+++ 02-practice/01_python/01_basics.py
@@ -5,3 +5,28 @@
 22 // 8  # Integer division -> 2
 8 / 2  # Division -> 4.0
 3 * 3  # Multiplication -> 9
+5 - 2  # Subtraction -> 3
+5 + 2  # Addition -> 7
+
+
+# --------------- Augmented Assignment Operators --------------- #
+
+var = 10
+var += 1  # var = var + 1
+var -= 1  # var = var - 1
+var *= 1  # var = var * 1
+var /= 1  # var = var / 1
+var //= 1  # var = var // 1
+var %= 1  # var = var % 1
+
+
+# --------------------- Test of Emptiness ---------------------- #
+a = [1, 2, 3]
+
+# Bad way:
+if len(a) > 0:
+    print("The list is not empty!")
+
+# Good way:
+if a:  # If a were empty, this was going to evaluate to "False"
+    print("The list is not empty!")
