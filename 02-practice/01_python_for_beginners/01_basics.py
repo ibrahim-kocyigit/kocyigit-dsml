83c28b41ee928f4e9f52a395479afa1566cc4f56
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-05-05 15:57:40 +0300

    added 01_basics and 02_operators to python_for_beginners

---
+++ 02-practice/01_python_for_beginners/01_basics.py
@@ -0,0 +1,70 @@
+# ------------------------- Variables ------------------------- #
+name = "Beau"
+age = 39
+pi = 3.14
+my_name = "Ibrahim"
+NAME = "Constant"
+
+# cannot start with a number
+1st_name = "Ibrahim" # SyntaxError: invalid decimal literal
+
+# cannot use Python keywords as variables
+
+if = "ibrahim" # -> SyntaxError: invalid syntax 
+
+
+# ----------------- Expressions and Statements ----------------- #
+# Expression returns a value
+"Ibrahim" # is an expression, returns "Ibrahim"
+
+# Statement is an operation on a value
+name = "Ibrahim" # is an operation: 'assignment to a variable'
+print(name) 
+
+# A program is formed by a series of statements.
+# This is a program:
+name = input("Please enter your name: ")
+print(f"Hell0 {name}")
+
+
+# -------------------------- Comments -------------------------- #
+# This is a comment line.
+name = "Ibrahim" # This is an inline comment. 
+'''
+This is a comment
+written in
+more than just one line
+'''
+
+# ------------------------- Data Types ------------------------- #
+name = "Ibrahim"
+lucky_number = 8
+pi = 3.14
+
+type(name) # -> str (string)
+type(lucky_number) # -> int (integer)
+type(pi) # -> float (floating number)
+
+print(type(name) == str) # -> True
+print(isinstance(lucky_number, float)) # -> False
+
+lucky_number = float(lucky_number) # Converting to float
+print(lucky_number) # -> 8.0
+print(isinstance(lucky_number, float)) # True
+
+# Casting
+age = "42"
+age = int(age)
+print(isinstance(age, int)) # True
+
+text = "ibrahim"
+text = int(text) # ValueError: invalid literal for int() with base 10: 'ibrahim'
+
+# Other data types
+complex # for complex numbers
+bool # for booleans (True or False)
+list # for lists
+tuple # for tuples
+range # for ranges
+dict # for dictionaries
+set # for sets
---
+++ 02-practice/01_python_for_beginners/02_operators.py
@@ -0,0 +1,76 @@
+# -------------------- Assignment Operators -------------------- #
+var = 10
+var += 1  # var = var + 1
+var -= 1  # var = var - 1
+var *= 1  # var = var * 1
+var /= 1  # var = var / 1
+var //= 1  # var = var // 1
+var %= 1  # var = var % 1
+
+
+# -------------------- Arithmetic Operators -------------------- #
+# From highest to lowers precedence:
+print(2**3)  # Exponent -> 8
+print(22 % 8)  # Modulus/Remainder -> 6
+print(22 // 8)  # Integer division -> 2
+print(8 / 2)  # Division -> 4.0
+print(3 * 3)  # Multiplication -> 9
+print(5 - 2)  # Subtraction -> 3
+print(5 + 2)  # Addition -> 7
+
+
+# -------------------- Comparison Operators -------------------- #
+"""
+==	Equal to
+!=	Not equal to
+<	Less than
+>	Greater Than
+<=	Less than or Equal to
+>=	Greater than or Equal to
+"""
+
+print("Hello" == "hello")  # -> False
+print(42 == 42.0)  # -> True
+print(42 == "42")  # -> False
+
+
+# --------------------- Boolean Operators --------------------- #
+print(True and True)  # -> True
+print(True and False)  # -> False
+print(False and True)  # -> False
+print(False and False)  # -> False
+
+print(True or True)  # -> True
+print(True or False)  # -> True
+print(False or True)  # -> True
+print(False or False)  # -> False
+
+print(not True)  # -> False
+print(not False)  # -> True
+
+# `or` operator returns the first True value
+print("hi" or "hey")  # -> 'hi'
+print(0 == 3 or "Hello")  # -> "Hello"
+
+# `and` operator evaluates the second argument only if the first argument is True
+print(0 != 3 and "Hi there!")  # -> "Hi there!"
+print("Hello everyone!" or "What's up?")  # -> "Hello everyone!"
+
+
+# -------------------- is & in Operators --------------------- #
+# `is` is the identity operator. returns 'True' if both are the same objects
+print(dict is dict)  # -> True
+
+# `in` is the membership operator. Tells if a value is contained within a list or another sequence
+exam_scores = [34, 46, 67]
+print(34 in exam_scores)  # -> True
+
+
+# -------------------- Ternary Operator ---------------------- #
+# Allows us to quickly define a conditional
+def is_adult(age):
+    return True if age >= 18 else False
+
+
+is_adult(15)  # -> False
+is_adult(25)  # -> True
