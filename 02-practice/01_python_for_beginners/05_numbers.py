910a67c1ec5d22cf77f2334d7ebddada8a6ffec0
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-05-07 19:44:19 +0300

    finished lists.py in python for beginners

--- 02-practice/01_python_for_beginners/05_numbers.py
+++ 02-practice/01_python_for_beginners/05_numbers.py
@@ -2,4 +2,36 @@
 # i^2 = -1 (i is the imaginary unit)
 # z = x + iy (all complex number can be written like this)
 
-# https://youtu.be/eWRfhZUzrAc?t=5201
+num = 2 + 3j
+type(num)  # -> complex
+
+print(num.real)  # -> 2.0
+print(num.imag)  # -> 3.0
+
+
+# ----------------------------- Built-in Functions ----------------------------- #
+abs(-5.5)  # -> 5.5
+round(5.49)  # -> 5
+round(5.49, 1)  # 5.5
+
+
+# -------------------------------- Enumarations -------------------------------- #
+# Enum is a set of symbolic names (members) bound to unique values
+from enum import Enum
+
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+
+print(Color.RED)  # -> Color.RED
+print(Color.BLUE.value)  # -> 3
+print(Color(2))  # -> Color
+print(Color["BLUE"])  # -> Color.BLUE
+
+Color.GREEN.value = 1  # Won't work!
+
+print(list(Color))  # -> [<Color.RED: 1>, <Color.GREEN: 2>, <Color.BLUE: 3>]
+print(len(Color))  # -> 3
