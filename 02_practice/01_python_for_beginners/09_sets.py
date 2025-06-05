b21b8b52cb19b72655bf16628f7f011d87a8e67e
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-06-05 19:00:50 +0300

    added sets.py and functions.py to 01_python_for_beginners

--- 02_practice/01_python_for_beginners/09_sets.py
+++ 02_practice/01_python_for_beginners/09_sets.py
@@ -1,1 +1,25 @@
 # ----------------------------------- Basics ----------------------------------- #
+# Sets are like dictionaries without keys.
+
+names_1 = {"Ibrahim", "Aykut"}
+names_2 = {"Ibrahim"}
+names_3 = {"John"}
+
+intersect = names_1 & names_2
+print(intersect)  # -> {'Ibrahim'}
+
+mod = names_1 | names_3
+print(mod)  # -> {'Ibrahim', 'Aykut', 'John'}
+
+difference = names_1 - names_2
+print(difference)  # -> {'Aykut'}
+
+is_superset = names_1 > names_2
+print(is_superset)  # -> True
+
+is_subset = names_2 < names_1
+print(is_subset)  # -> True
+
+# Sets cannot have two of the same item:
+names_3 = {"John", "John"}
+print(names_3)  # -> {'John'}
---
+++ 02_practice/01_python_for_beginners/10_functions.py
@@ -0,0 +1,110 @@
+# ----------------------------------- Basics ----------------------------------- #
+# A function is a block of organized code that is used to perform a single task. They provide better modularity for your application and reuse-ability.
+def hello():
+    print("Hello!")
+
+
+hello()  # -> Hello!
+
+
+# Function Arguments:
+def say_hello(name):
+    print(f"Hello {name}!")
+
+
+say_hello("Ibrahim")  # -> Hello Ibrahim!
+say_hello(name="Aykut")  # -> Hello Aykut!
+
+
+# Return Values:
+def add(number_1, number_2):
+    return number_1 + number_2
+
+
+total = add(2, 3)
+print(total)  # -> 5
+
+
+# --------------------------- Local and Global Scope --------------------------- #
+# Code in the global scope cannot use any local variables. However, a local scope can access global variables.
+# Code in a function’s local scope cannot use variables in any other local scope.
+# You can use the same name for different variables if they are in different scopes. That is, there can be a local variable named spam and a global variable also named spam.
+global_variable = "I am available everywhere"
+
+
+def some_function():
+    print(global_variable)  # because is global
+    local_variable = "Only available within this function"
+
+
+print(local_variable)  # raises NameError
+
+# If you need to modify a global variable from within a function, use the global statement:
+
+
+def spam():
+    global eggs
+    eggs = "spam"
+
+
+eggs = "global"
+spam()
+print(eggs)  # -> spam
+
+
+# ------------------------------ Nested Functions ------------------------------ #
+# Functions nested in another function is only visible inside that function. They are used to create utilities that are useful for only the main function
+def talk(phrase):
+    def say(word):
+        print(word)
+
+    words = phrase.split(" ")
+    for word in words:
+        say(word)
+
+
+talk("I am going.")
+"""
+I
+am
+going.
+"""
+
+
+# If we want to access a variable declared inside the outer function from the inner function, we have to use the keyword nonlocal
+def count():
+    count = 0
+
+    def increment():
+        nonlocal count
+        count = count + 1
+        print(count)
+
+    increment()
+
+
+count()  # -> 1
+
+# ----------------------------------- Closure ---------------------------------- #
+# A closure allows a function to remember and access variables from its lexical scope, even when the function is executed outside that scope.
+
+
+def counter():
+    count = 0
+
+    def increment():
+        nonlocal count
+        count = count + 1
+        return count
+
+    return increment
+
+
+increment = counter()
+
+print(increment())  # -> 1
+print(increment())  # -> 2
+print(increment())  # -> 3
+
+# ------------------------------ Lambda Functions ------------------------------ #
+# Lambda functions are useful when combined with other functions such as map, filter, and reduce. Check that section for more information.
