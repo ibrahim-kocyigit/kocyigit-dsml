49806d827fa45a031ccbb1d491265965f99b6c52
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-10-31 14:59:40 +0300

    restructuring 02_linear_and_logistic_regression

--- 03_specializations/02_machine_learning_with_python/02_linear_and_logistic_regression/03_implementation--simple_linear_regression.py
+++ 03_specializations/02_machine_learning_with_python/02_linear_and_logistic_regression/03_implementation--simple_linear_regression.py
@@ -4,27 +4,28 @@ import numpy as np
 
 class SimpleLinearRegression:
     def __init__(self):
-        self.intercept = None
         self.slope = None
+        self.intercept = None
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> SimpleLinearRegression:
-        # The slope: rise / run = cov(X, y) / var(X)
+        # Slope: rise / run = cov(X, y) / var(X)
         self.slope = np.cov(X, y, bias=True)[0, 1] / np.var(X)
 
         """
-        # The alternative formula (from the lecture):
-        self.slope = np.sum((X - np.mean(X)) * (y - np.mean(y))) / np.sum((X - np.mean(X)) ** 2)
+        # Alternatively, we can use the formula from the lecture:
+        self.slope = np.sum((X - np.mean(X)) * (y - np.mean(y))) / np.sum(
+            ((X - np.mean(X)) ** 2)
+        )
         """
 
-        # The intercept: y_bar - slope * x_bar
+        # Intercept: y_bar - slope * x_bar
         self.intercept = np.mean(y) - self.slope * np.mean(X)
 
-        return self  # So that we can chain the calls
+        return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        if self.intercept is None or self.slope is None:
+        if self.slope is None or self.intercept is None:
             raise ValueError("Model is not trained yet. Please call 'fit' first.")
-
         return self.slope * X + self.intercept
 
 
