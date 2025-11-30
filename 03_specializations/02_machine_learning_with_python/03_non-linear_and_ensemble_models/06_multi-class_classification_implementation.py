cc7519e8986d8ddb3c3c16c6e07b16314e850b8a
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-11-30 15:21:04 +0300

    working on 07_multi-class_classification_lab

--- 03_specializations/02_machine_learning_with_python/03_non-linear_and_ensemble_models/06_multi-class_classification_implementation.py
+++ 03_specializations/02_machine_learning_with_python/03_non-linear_and_ensemble_models/06_multi-class_classification_implementation.py
@@ -52,13 +52,13 @@ class LogisticClassifier:
         Returns:
             LogisticClassifier: The trained classifier instance.
         """
-        n_observations, n_features = X.shape
+        n_samples, n_features = X.shape
         self.weights = np.zeros(n_features)
         self.bias = 0
         for _ in range(self.max_iters):
             p_hat = sigmoid(X @ self.weights + self.bias)
-            dw = (1 / n_observations) * X.T @ (p_hat - y)
-            db = (1 / n_observations) * np.sum(p_hat - y)
+            dw = (1 / n_samples) * X.T @ (p_hat - y)
+            db = (1 / n_samples) * np.sum(p_hat - y)
             self.weights = self.weights - self.learning_rate * dw
             self.bias = self.bias - self.learning_rate * db
         return self
